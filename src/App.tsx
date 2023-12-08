48371679a834f3aa693d86a7b41953c59c7f32b6
stron <yaneliyahu83@gmail.com>
2023-12-08 17:11:01 +0200

    chore: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { SettingsBar } from "./components/SettingsBar/SettingsBar/SettingsBar";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <main className="app-container">
+      <SettingsBar />
+      <div className="app-container__content">
+        <Routes>
+          <Route path="/" Component={HomePage} />
+        </Routes>
+      </div>
+    </main>
+  );
 }
 
 export default App;
