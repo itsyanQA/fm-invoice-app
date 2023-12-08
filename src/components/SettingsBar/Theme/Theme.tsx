ddc8f62beef24426dea99c4197a6b327aa9d0235
stron <yaneliyahu83@gmail.com>
2023-12-08 19:49:27 +0200

    feat: add @prefers-color-scheme media query

--- src/components/SettingsBar/Theme/Theme.tsx
+++ src/components/SettingsBar/Theme/Theme.tsx
@@ -3,15 +3,30 @@ import { ReactComponent as MoonIcon } from "../../../assets/svgs/icon-moon.svg";
 import { ReactComponent as SunIcon } from "../../../assets/svgs/icon-sun.svg";
 import "./Theme.scss";
 
+type ThemeMode = "light" | "dark";
+
+const getInitialTheme = (): ThemeMode => {
+  if (
+    localStorage.getItem("theme") === "dark" ||
+    (window.matchMedia("(prefers-color-scheme: dark)") && localStorage.getItem("theme") !== "light")
+  ) {
+    return "dark";
+  }
+
+  return "light";
+};
+
 export function Theme() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [theme, setTheme] = useState<ThemeMode>(getInitialTheme);
 
   useEffect(() => {
     if (theme === "dark") {
       document.body.setAttribute("data-theme", "dark");
-    } else {
+    } else if (theme === "light") {
       document.body.setAttribute("data-theme", "light");
     }
+
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const clickHandler = (): void => {
