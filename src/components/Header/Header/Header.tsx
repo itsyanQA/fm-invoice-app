9527c403372922ba834b5a83d1d59b29f2518571
stron <yaneliyahu83@gmail.com>
2023-12-23 14:22:53 +0200

    style: modify _mixins.scss and _colors.scss files

--- src/components/Header/Header/Header.tsx
+++ src/components/Header/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Filter } from "../Filter/Filter";
 import { HeaderHeadings } from "../HeaderHeadings/HeaderHeadings";
 import { HeaderNewInvoiceButton } from "../HeaderNewInvoiceButton/HeaderNewInvoiceButton";
 import "./Header.scss";
@@ -10,7 +11,10 @@ export function Header({ invoicesCount }: HeaderProps) {
   return (
     <header className="header">
       <HeaderHeadings invoicesCount={invoicesCount} />
-      <HeaderNewInvoiceButton />
+      <div className="header__filter-and-button-container">
+        <Filter />
+        <HeaderNewInvoiceButton />
+      </div>
     </header>
   );
 }
