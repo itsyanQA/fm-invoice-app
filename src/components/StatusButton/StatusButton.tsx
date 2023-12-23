327edb7c8fa200bd9535102db9fa6e9aee3b2b74
stron <yaneliyahu83@gmail.com>
2023-12-23 14:21:22 +0200

    feat: add StatusButton.tsx component

---
+++ src/components/StatusButton/StatusButton.tsx
@@ -0,0 +1,22 @@
+import { Status } from "../../types/types";
+import { ReactComponent as Bullet } from "../../assets/svgs/bullet.svg";
+import "./StatusButton.scss";
+
+type StatusButtonProps = {
+  status: Status;
+};
+
+export function StatusButton({ status }: StatusButtonProps) {
+  const statusButtonClassModifier: Record<Status, string> = {
+    draft: "status-button--is-draft",
+    paid: "status-button--is-paid",
+    pending: "status-button--is-pending",
+  };
+
+  return (
+    <button className={`status-button ${statusButtonClassModifier[status]}`}>
+      <Bullet className="status-button__bullet" />
+      <span className="status-button__text">{status}</span>
+    </button>
+  );
+}
