03d7385293da220564851b79c212605619006605
stron <yaneliyahu83@gmail.com>
2023-12-23 14:21:02 +0200

    style: add more css properties to StyledSelect.ts component

--- src/styled/StyledSelect.ts
+++ src/styled/StyledSelect.ts
@@ -5,4 +5,16 @@ export const StyledSelect = styled(Select)(({ theme }) => ({
   "& fieldset": {
     border: "unset",
   },
+  "& .MuiSelect-select": {
+    padding: "7px 30px 10px 0px!important",
+    fontSize: "15px",
+    fontWeight: "700",
+    letterSpacing: "-0.25px",
+  },
+  "&.filter-select--is-selected": {
+    textTransform: "capitalize",
+  },
+  "& > svg": {
+    top: "15px !important",
+  },
 }));
