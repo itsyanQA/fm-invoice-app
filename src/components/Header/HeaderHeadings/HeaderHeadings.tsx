cc83bd3c81729f863279436cce85087ab8aa8896
stron <yaneliyahu83@gmail.com>
2023-12-23 14:27:57 +0200

    refactor: modify invoice count text to include no invoices

--- src/components/Header/HeaderHeadings/HeaderHeadings.tsx
+++ src/components/Header/HeaderHeadings/HeaderHeadings.tsx
@@ -8,13 +8,19 @@ type HeadingHeadingsProps = {
 export function HeaderHeadings({ invoicesCount }: HeadingHeadingsProps) {
   const isMobile = useIsMobile();
 
+  const getInvoiceCountText = () => {
+    if (isMobile) {
+      return `${invoicesCount} invoices`;
+    }
+
+    return `There are ${invoicesCount} total invoices`;
+  };
+
   return (
     <div className="header-title-container">
       <h1 className="header-title-container__title">Invoices</h1>
       {!!invoicesCount && (
-        <span className="header-title-container__invoices-count">
-          {isMobile ? `${invoicesCount} invoices` : `There are ${invoicesCount} total invoices`}
-        </span>
+        <span className="header-title-container__invoices-count">{invoicesCount ? getInvoiceCountText() : "No invoices"}</span>
       )}
     </div>
   );
