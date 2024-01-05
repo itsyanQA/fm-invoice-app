db165650317b42b28e7d790dca1602dcf66e5652
stron <yaneliyahu83@gmail.com>
2024-01-05 17:49:58 +0200

    refactor: create handleThemeChange fn in Theme.tsx

--- src/components/SettingsBar/Theme/Theme.tsx
+++ src/components/SettingsBar/Theme/Theme.tsx
@@ -1,25 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ReactComponent as MoonIcon } from "../../../assets/svg/icon-moon.svg";
 import { ReactComponent as SunIcon } from "../../../assets/svg/icon-sun.svg";
 import "./Theme.scss";
+import { useDidMountEffect } from "../../../hooks/useDidMountEffect";
 
 type ThemeMode = "light" | "dark";
 
-const getInitialTheme = (): ThemeMode => {
-  if (
-    localStorage.getItem("theme") === "dark" ||
-    (window.matchMedia("(prefers-color-scheme: dark)") && localStorage.getItem("theme") !== "light")
-  ) {
-    return "dark";
-  }
-
-  return "light";
-};
-
 export function Theme() {
-  const [theme, setTheme] = useState<ThemeMode>(getInitialTheme);
-
-  useEffect(() => {
+  const handleThemeChange = (theme: ThemeMode) => {
     if (theme === "dark") {
       document.body.setAttribute("data-theme", "dark");
     } else if (theme === "light") {
@@ -27,7 +15,24 @@ export function Theme() {
     }
 
     localStorage.setItem("theme", theme);
-  }, [theme]);
+  };
+
+  const getInitialTheme = (): ThemeMode => {
+    if (
+      localStorage.getItem("theme") === "dark" ||
+      (window.matchMedia("(prefers-color-scheme: dark)") && localStorage.getItem("theme") !== "light")
+    ) {
+      handleThemeChange("dark");
+      return "dark";
+    }
+
+    handleThemeChange("light");
+    return "light";
+  };
+
+  const [theme, setTheme] = useState<ThemeMode>(getInitialTheme);
+
+  useDidMountEffect(() => handleThemeChange(theme), [theme]);
 
   const clickHandler = (): void => {
     setTheme((currentTheme) => {
